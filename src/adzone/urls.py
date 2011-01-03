e6fb8312785584895a2f6f68388d2ef1188fe5aa
Colin Powell <powellc@saddleback.(none)>
2011-01-03 11:23:09 -0500

    Adding a hook url to show ads in frames on legacy pages.

--- src/adzone/urls.py
+++ src/adzone/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
 
-from adzone.views import ad_view
+from adzone.views import ad_view, ad_display
 
 urlpatterns = patterns('',
     url(r'^view/(?P<id>[\d]+)/$', ad_view, name='adzone_ad_view'),
+    url(r'^addisplay.html', ad_display, name='adzone_ad_display'),
 )
